3666ee98bcf173b3b52931b134423ac8ed0c9b1b
Jordan Smyth <jsmyth1423@gmail.com>
2022-07-25 19:46:18 +0100

    initial commit

---
+++ ecommerce_project/store/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Cart, CartItem, Category, Product
+
+
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(CartItem)
