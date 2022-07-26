6d44f907368fd1fe36178cbe30c51892adb85104
Jordan Smyth <jsmyth1423@gmail.com>
2022-07-26 17:05:36 +0100

    added customer mode, login logout, signin signout, search functions

--- ecommerce_project/ecommerce_project/settings.py
+++ ecommerce_project/ecommerce_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- ecommerce_project/store/urls.py
+++ ecommerce_project/store/urls.py
@@ -6,10 +6,14 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('category/<slug:category_slug>', views.home, name='products_by_category'),
     path('category/<slug:category_slug>/<slug:product_slug>', views.productPage, name='product_detail'),
+    path('cart', views.cart_detail, name='cart_detail'),
     path('cart/add/<int:product_id>', views.add_cart, name='add_cart'),
     path('cart/remove/<int:product_id>', views.cart_remove, name='cart_remove'),
     path('cart/remove_product/<int:product_id>', views.cart_remove_product, name='cart_remove_product'),
-    path('cart', views.cart_detail, name='cart_detail'),
+    path('account/create/', views.signupView, name='signup'),
+    path('account/signin/', views.signinView, name='signin'),
+    path('account/signout/', views.signoutView, name='signout'),
+    path('search/', views.search, name='search'),
     
 
 ]
